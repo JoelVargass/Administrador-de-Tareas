cf60063f7cb3dd90b3d2b632637be41872390be8
JoelVargass <jovap723@gmail.com>
2025-02-02 17:29:01 -0500

    Inicio

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container mx-auto px-4 py-8 max-w-3xl '>
+          
+      </div>
+    </>
+  )
+}
+
+export default App
